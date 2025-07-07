37c0acbb2ec7d85379f5c836508e2ec8ea4cc61a
Dan Hydra <dan@hydracloud.key>
2025-07-07 08:48:55 +0100

    Change to macOS26

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AutoScreenshooter",
     platforms: [
-        .macOS(.v13)
+        .macOS("26.0")
     ],
     products: [
         .executable(
